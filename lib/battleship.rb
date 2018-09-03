936d67f212dca03be6d2f0a64c0c17166bb62f3d
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-03 09:21:09 -0600

    get response from player to start game

---
+++ lib/battleship.rb
@@ -0,0 +1,16 @@
+
+puts "Welcome to BATTLESHIP \n\n"
+print "Would you like to (p)lay, read the (i)nstructions, or (q)uit?
+>"
+
+response = gets.chomp.downcase
+
+if response ==  "p"
+  game = Game.new
+elsif
+  response == "i"
+    "These are the instructions for now"
+else
+  response == "q"
+  p "Game over"
+end
