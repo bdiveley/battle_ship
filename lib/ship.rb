a20076e759037c2d9661095d1d218c3fc952e8c5
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-03 11:57:06 -0600

    testing randomization of comp coords

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,76 @@
 class Ship
-    attr_reader :length
+    attr_reader :length,
+                :coordinates
 
   def initialize(length)
     @length = length
+    @coordinates = []
+  end
+
+  def randomize
+    ["A", "B", "C", "D"].sample +
+    ["1", "2", "3","4"].sample
+  end
+
+  def verify_first_coord
+    if @length == 2
+      first = "D4"
+      while first == "D4"
+        first = randomize
+      end
+    else
+      first = "D4"
+      while first == "C3" || first == "C4" ||
+            first == "D3" || first == "D4"
+        first = randomize
+      end
+    end
+      @coordinates << first
+      return first
+  end
+
+  def random_orientation
+    first = @coordinates[0]
+    orientation = ["vertical", "horizontal"].sample
+    if @length == 2
+      orientation = "horizontal" if first[0] == "D"
+      orientation = "vertical" if first[1] == "4"
+    else
+      if first[0] == "D" || first[0] == "C"
+        orientation = "horizontal"
+      elsif first[1] == "3" || first[1] == "4"
+        orientation = "vertical"
+      end
+    end
+    return orientation
+  end
+
+  def verify_next_coord
+    orientation = random_orientation
+    index = 0
+
+    binding.pry
+    while @coordinates.count < @length.to_i
+      if orientation == "vertical"
+        next = alpha_compares(alpha_compares(@coordinates[index][0]) + 1) + @coordinates[index][-1]
+      elsif
+        next = @coordinates[index][0] + (@coordinates[index][1].to_i + 1).to_s
+      end
+      @coordinates << next
+      index += 1
+    end
+  end
+
+  def alpha_compares(coord)
+    key = {"A" => 1, "B" => 2, "C" => 3, "D" => 4}
+    if coord.class == String
+      key[coord]
+    else
+      alpha = key.find do |num|
+        num[1] == coord
+      end
+      return alpha[0]
+    end
   end
 
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,6 +2,7 @@ require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
+require 'pry'
 
 class ShipTest < Minitest::Test
 
@@ -18,4 +19,99 @@ class ShipTest < Minitest::Test
     assert_equal "2", ship.length
     assert_equal "3", ship_2.length
   end
+
+  def test_it_starts_with_no_coordinates
+    ship = Ship.new("3")
+
+    assert_equal [], ship.coordinates
+  end
+
+  def test_player_can_randomize_a_coordinate
+    ship = Ship.new("2")
+
+    first_random = ship.randomize
+    second_random = ship.randomize
+
+    refute_equal first_random, second_random
+  end
+
+  def test_first_coord_not_D4_for_destroyer
+    ship = Ship.new("2")
+    100.times do
+      expected = ship.verify_first_coord
+      refute expected == "D4"
+    end
+  end
+
+  def test_first_coord_not_C3_D3_C4_D4_for_cruiser
+    ship = Ship.new("3")
+    100.times do
+      expected = ship.verify_first_coord
+      refute expected == "C3"
+      refute expected == "D3"
+      refute expected == "C4"
+      refute expected == "D4"
+    end
+  end
+
+  def test_first_coord_added_to_array_after_verified
+    ship = Ship.new("3")
+    ship_2 = Ship.new("2")
+    ship.verify_first_coord
+    ship_2.verify_first_coord
+
+    assert_equal 1, ship.coordinates.count
+    assert_equal 1, ship_2.coordinates.count
+  end
+
+  def test_can_randomly_assign_orientation
+    ship = Ship.new("2")
+    ship.verify_first_coord
+
+    test = []
+    15.times do
+      test << ship.random_orientation
+    end
+    actual = test.all? do |orientation|
+      orientation == "vertical"
+    end
+    refute actual
+  end
+
+  def test_alpha_coords_return_nums
+    ship = Ship.new("2")
+
+    assert_equal 1, ship.alpha_compares("A")
+    assert_equal 2, ship.alpha_compares("B")
+    assert_equal 3, ship.alpha_compares("C")
+    assert_equal 4, ship.alpha_compares("D")
+  end
+
+  def test_number_coords_return_alpha
+    ship = Ship.new("2")
+
+    assert_equal "A", ship.alpha_compares(1)
+    assert_equal "B", ship.alpha_compares(2)
+    assert_equal "C", ship.alpha_compares(3)
+    assert_equal "D", ship.alpha_compares(4)
+  end
+
+  def test_can_return_second_coordinate_for_destroyer
+    ship = Ship.new("2")
+
+    ship.verify_first_coord
+    ship.verify_next_coord
+
+    assert_equal 2, ship.coordinates.count
+  end
+
+  def test_can_return_second_coordinate_for_cruiser
+    ship = Ship.new("3")
+
+    ship.verify_first_coord
+    ship.verify_next_coord
+
+    assert_equal 2, ship.coordinates.count
+  end
+
 end
