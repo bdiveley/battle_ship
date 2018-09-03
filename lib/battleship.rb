d064b431bf200d78fab0be8a4626d122a0291562
Bailey Diveley <bailey.diveley@gmail.com>
2018-09-03 15:16:06 -0600

    fixed formatting

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,6 +1,7 @@
 require './lib/game'
 require './lib/player'
 require './lib/ship'
+require 'pry'
 
 
 puts "Welcome to BATTLESHIP \n\n"
@@ -13,6 +14,7 @@ if response ==  "p"
   game = Game.new
   game.randomly_assign_coordinates
   game.get_coordinates
+
 
 elsif
   response == "i"
